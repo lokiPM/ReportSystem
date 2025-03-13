57ed1e26ed2290398aee9c9a7d76c7a532a183b6
lubro <142106680+lokiPM@users.noreply.github.com>
2025-03-13 18:14:41 +0100

    Added new Form Test

--- src/Main.php
+++ src/Main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
 
 class Main extends PluginBase {
 
@@ -19,9 +19,17 @@ class Main extends PluginBase {
     }
 
     public function openReportForm(Player $player): void {
-        $form = new SimpleForm(function (Player $player, ?int $data) {});
+        $form = new CustomForm(function (Player $player, ?array $data) {});
+
         $form->setTitle("Report a Player");
-        $form->setContent("Soon");
+        $form->addLabel("Player");
+
+        $onlinePlayers = [];
+        foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
+            $onlinePlayers[] = $onlinePlayer->getName();
+        }
+
+        $form->addDropdown("Select a Player", $onlinePlayers);
         $player->sendForm($form);
     }
 }
